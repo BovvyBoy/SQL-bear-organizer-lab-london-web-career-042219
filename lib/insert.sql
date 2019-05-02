1d2dd21bf16c13a70c9eda6d51c31af10e07c749
Daniel Bovington <d.bovington@gmail.com>
2019-05-02 13:44:09 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'male', 'black', 'violent', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 12, 'female', 'white', 'calm', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'female', 'brown', 'cool', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'male', 'black', 'violent', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'female', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 12, 'male', 'white', 'angry', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'female', 'brown', 'violent', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 6, 'male', 'black', 'violent', 1);
